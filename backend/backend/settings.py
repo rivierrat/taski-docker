13f05553ab82f885c4d6c3843173af89e7f9d366
Oleg Ageev <ageev@finux.ru>
2025-03-17 21:55:27 +0400

    broke ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'taski.finux.ru', '51.250.25.253', ]
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'taski.finux.ru', '51.250.25.253', ]
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
